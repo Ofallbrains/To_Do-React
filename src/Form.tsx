42b27b84ac12c96f172472c0179454da6f23f649
Ofallbrains <ofallbrains@gmail.com>
2025-08-07 13:18:32 +0200

    ToDo app in react

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
+import { IoIosAddCircle } from "react-icons/io";
 
-interface Todo{
+interface Todo {
     id: number;
     text: string;
     completed: boolean;
@@ -25,11 +26,69 @@ const Form: React.FC = () => {
             setTodos([...todos, todo])
             setNewTodo('')
         }
-}
+    }
 
-    
+    const toggleTodo = (id: number): void => {
+        setTodos(todos.map(todo =>
+            todo.id === id ? { ...todo, completed: !todo.completed } : todo
+        ))
+    }
+
+    const deleteTodo = (id: number): void => {
+        setTodos(todos.filter(todo => todo.id !== id));
+    }
+
+    const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+        if (e.key === 'Enter') {
+            addTodo();
+        }
+    };
+
     return (
-        <h1>wow</h1>
+        <div className='bg-red-900'>
+            <div>
+                <h1>todos</h1>
+            </div>
+
+            <div>
+                <input
+                    type="text"
+                    value={newTodo}
+                    onChange={(e) => setNewTodo(e.target.value)}
+                    onKeyPress={handleKeyPress}
+                    placeholder='Add todo...'
+                />
+
+                <button
+                    onClick={addTodo}
+                >   <IoIosAddCircle /></button>
+            </div>
+
+            <div>
+                {todos.map((todo) => (
+                    <div
+                        key={todo.id}
+
+                    >
+                        <input
+                            type='checked'
+                            checked={todo.completed}
+                            onChange={() => toggleTodo(todo.id)}
+
+                        />
+
+                        <span
+                            className={`flex-1 text-sm ${todo.completed
+                                    ? 'text-gray-400 line-through'
+                                    : 'text-gray-700'
+                                }`}
+                        >
+
+                        </span>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
